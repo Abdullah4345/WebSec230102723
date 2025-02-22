6ff0cb64e6362beeb5e6acd05af325bb4aeda1e1
Abdullah <133496519+Abdullah4345@users.noreply.github.com>
2025-02-22 20:27:01 +0200

    assignment 1

---
+++ WebSecService/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/even-numbers', function () {
+    return view('even_numbers');
+});
+Route::get('/prime-numbers', function () {
+    return view('prime_numbers');
+});
+Route::get('/multiplication-table', function () {
+    return view('multiplication_table');
+});
